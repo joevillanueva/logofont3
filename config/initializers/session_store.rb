6cc37213a0e48b18336740d97a8e3a2993461874
joevillanueva <joe@gophilosophie.com>
2013-10-02 15:01:54 -0700

    Initial commit.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Logofonts::Application.config.session_store :cookie_store, key: '_logofonts_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Logofonts::Application.config.session_store :active_record_store
